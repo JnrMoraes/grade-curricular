54f7b97d12bde6db5e08a121d1772fe907f51f2d
juniormoraes <contatomoraesjr@gmail.com>
2020-11-24 19:37:17 -0300

    6 Separate service layer responsibilities

--- src/main/java/br/cliente/escola/gradecurricular/controller/MateriaController.java
+++ src/main/java/br/cliente/escola/gradecurricular/controller/MateriaController.java
@@ -6,10 +6,12 @@ import br.cliente.escola.gradecurricular.repository.IMateriaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -41,6 +43,17 @@ public class MateriaController {
         return ResponseEntity.status(HttpStatus.OK).body(this.iMateriaRepository.findById(id).get()); // chamo o Optiona e depois dou um get nele, ver o Sonar
     }
 
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Boolean> excluirMateria(@PathVariable Long id){
+        try{
+            this.iMateriaRepository.deleteById(id);
+            return ResponseEntity.status(HttpStatus.OK).body(true);
+
+        } catch (Exception e){
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(false);
+        }
+    }
+
     @PostMapping
     public ResponseEntity<Boolean> cadastrarMateria(@RequestBody MateriaEntity materiaEntity){ // json, corpo para o webservice
         try{
@@ -52,5 +65,25 @@ public class MateriaController {
 
     }
 
+    @PutMapping ResponseEntity<Boolean> atualizarMateria(@RequestBody MateriaEntity materiaEntity){
+        try{
+            // busca a materia que quero atualizar e guardo em uma variável
+          MateriaEntity materiaEntityAtualizada = this.iMateriaRepository.findById(materiaEntity.getId()).get();
 
+            // atualizo todos os valores
+            materiaEntityAtualizada.setNome(materiaEntity.getNome());
+            materiaEntityAtualizada.setCodigo(materiaEntity.getCodigo());
+            materiaEntityAtualizada.setHoras(materiaEntity.getHoras());
+            materiaEntityAtualizada.setFrequencia(materiaEntity.getFrequencia());
+
+            // salva as alterações feitas
+            this.iMateriaRepository.save(materiaEntityAtualizada);
+
+            return ResponseEntity.status(HttpStatus.OK).body(true);
+
+        } catch (Exception e){
+
+            return  ResponseEntity.status(HttpStatus.BAD_REQUEST).body(false);
+        }
+    }
 }
